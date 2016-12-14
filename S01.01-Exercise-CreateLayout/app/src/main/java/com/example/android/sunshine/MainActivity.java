ba97086bb60036443d02a4284f3a3b2688be6084
tavano <tavanojason@gmail.com>
2016-12-14 13:03:50 -0800

    Initial Test4

--- S01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     //test
     //test
     //test
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
